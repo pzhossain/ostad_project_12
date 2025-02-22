0b338ee3ef42a93c82cfd2e8bddd9146404baeb4
Parvez <pzhossain@gmail.com>
2025-02-22 07:25:15 +0600

    Final upload

---
+++ script.js
@@ -0,0 +1,104 @@
+// Selecting necessary elements
+
+const newTaskInput = document.getElementById("new-task"); //field for new task
+const addTaskButton = document.getElementById("addTask"); //add/update task
+const incompleteTaskList = document.getElementById("items"); //incomplete tasks
+const completeTaskList = document.querySelector(".complete-list ul"); //completed tasks
+
+
+let editingTask = null;
+
+
+function createTaskElement(taskText) {
+    // Li Creation
+    const li = document.createElement("li");
+    console.log(li);
+    li.classList.add("item");
+  
+    // Checkbox creation
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    // complete event fire
+    checkbox.addEventListener("change", completeTask);
+  
+    // Label Creation
+    const label = document.createElement("label");
+    label.textContent = taskText;
+  
+    // Edit Button Creation
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.classList.add("edit");
+    // edit event fire
+    editButton.addEventListener("click", editTask);
+  
+    // add element to the list item (li)
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(editButton);
+  
+    return li;
+  }
+
+
+  function addOrUpdateTask(event) {
+    // Prevent browser reload
+    event.preventDefault();
+  
+    //   Get the task text
+    const taskText = newTaskInput.value.trim();
+    //  if input is empty do nothing
+    if (taskText === "") return;
+  
+    if (editingTask) {
+      // editing task
+      editingTask.querySelector("label").textContent = taskText;
+      addTaskButton.value = "Add Task";
+      editingTask = null;
+    } else {
+      // Add new task
+      const listItem = createTaskElement(taskText);
+      incompleteTaskList.appendChild(listItem);
+    }
+  
+    newTaskInput.value = ""; //Clear the input field
+  }
+
+
+  addTaskButton.addEventListener("click", addOrUpdateTask);
+
+
+  function editTask() {
+    const listItem = this.parentElement; // get the parent(li)
+    const label = listItem.querySelector("label"); // get label
+    //   Populate the new task input field with the incomplete task text
+    newTaskInput.value = label.textContent;
+  
+    //   Update the add task button to update task
+    addTaskButton.value = "Update Task";
+    editingTask = listItem; // set the editing mode
+  }
+  
+  // Function to mark a task as completed
+  function completeTask() {
+    const listItem = this.parentElement; // get the parent (li)
+    this.remove(); //Remove the checkbox when it is checked as completed
+    listItem.querySelector(".edit").remove(); // remove the edit button to the completed panel
+  
+    //  Delete Button Creation
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete");
+    deleteButton.addEventListener("click", deleteTask);
+  
+    listItem.appendChild(deleteButton); // add the delete button to the completed panel's li
+  
+    //Add the task item to completed panel
+    completeTaskList.appendChild(listItem);
+  }
+  
+  // Function to delete a completed task
+  function deleteTask() {
+    const listItem = this.parentElement; // get parent (li)
+    listItem.remove();
+  }
